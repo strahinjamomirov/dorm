c0f019dde770c8589df51ab94197a72e6cc03e9c
strahinja <strahinjamomirov@gmail.com>
2018-11-29 20:05:47 +0100

    refactor(Deploy)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,23 +4,8 @@
 
 use common\components\Box;
 
-$this->title = 'Dorm Change';
+$this->title = 'Stop Tackanju';
 ?>
 <div class="site-index">
-
-    <?php Box::begin([
-        'encodeLabel' => false,
-        'collapsable' => false
-    ]); ?>
-
-    <p style="font-size: 1cm">
-        UKOLIKO NEKO ZELI DA MENJA ZENSKO MESTO CETVRTI APRIL A/B BLOK ZA STUDENJAK 3 BLOK NEKA SE JAVI:
-
-        <strong>https://www.facebook.com/strahinja.momirov</strong>
-    </p>
-
-    <?php
-    Box::end();
-    ?>
 
 </div>
