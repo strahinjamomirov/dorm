494a3d839615e6559911890b981891a36ba0e324
strahinja <strahinjamomirov@gmail.com>
2018-11-29 20:11:40 +0100

    refactor(Deploy)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,9 +3,29 @@
 /* @var $this yii\web\View */
 
 use common\components\Box;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'Stop Tackanju';
 ?>
 <div class="site-index">
+
+    <?php Box::begin([
+        'encodeLabel' => false,
+        'collapsable' => false
+    ]); ?>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6">
+            <?= Html::a('Menjanje domova', Url::to(['changing-dorm/index']), ['class' => 'btn btn-primary btn-block btn-flat']) ?>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <?= Html::a('Pitanja', Url::to(['question/index']), ['class' => 'btn btn-primary btn-block btn-flat']) ?>
+        </div>
+    </div>
+
+    <?php
+    Box::end();
+    ?>
 
 </div>
